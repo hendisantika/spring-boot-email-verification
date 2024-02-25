b7f6aa223f48da246a2577bc88be07e925d95987
hendisantika <hendisantika@yahoo.co.id>
2024-02-25 08:19:38 +0700

    Add CustomUserDetailsService 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootemailverification/service/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package id.my.hendisantika.springbootemailverification.service;
+
+import id.my.hendisantika.springbootemailverification.entity.CustomUserDetails;
+import id.my.hendisantika.springbootemailverification.entity.User;
+import id.my.hendisantika.springbootemailverification.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-email-verification
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/25/24
+ * Time: 08:19
+ * To change this template use File | Settings | File Templates.
+ */
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new CustomUserDetails(user);
+    }
+}
