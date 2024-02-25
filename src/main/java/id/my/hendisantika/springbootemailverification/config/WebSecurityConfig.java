7db472e762fed769811afdd38ca84cbc835c7ca0
hendisantika <hendisantika@yahoo.co.id>
2024-02-25 08:18:11 +0700

    Add configure bean in WebSecurityConfig 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootemailverification/config/WebSecurityConfig.java
+++ src/main/java/id/my/hendisantika/springbootemailverification/config/WebSecurityConfig.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
@@ -47,7 +49,22 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    protected SecurityFilterChain configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authenticationProvider());
     }
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/users").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .usernameParameter("email")
+                .defaultSuccessUrl("/users")
+                .permitAll()
+                .and()
+                .logout().logoutSuccessUrl("/").permitAll();
+    }
+
 }
