ff2c70635bfc5cb231e10bde897040cc45c92059
hendisantika <hendisantika@yahoo.co.id>
2024-02-25 08:10:59 +0700

    Add findByEmail & findByVerificationCode in UserRepository 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootemailverification/repository/UserRepository.java
+++ src/main/java/id/my/hendisantika/springbootemailverification/repository/UserRepository.java
@@ -2,6 +2,7 @@ package id.my.hendisantika.springbootemailverification.repository;
 
 import id.my.hendisantika.springbootemailverification.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,4 +15,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * To change this template use File | Settings | File Templates.
  */
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    User findByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.verificationCode = ?1")
+    User findByVerificationCode(String code);
 }
