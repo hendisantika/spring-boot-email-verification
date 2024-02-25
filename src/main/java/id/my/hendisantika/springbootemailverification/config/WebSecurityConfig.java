ee4c5505471b4ae4e147352c253fb105850d5788
hendisantika <hendisantika@yahoo.co.id>
2024-02-25 08:15:35 +0700

    Add WebSecurityConfig 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootemailverification/config/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.springbootemailverification.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-email-verification
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/25/24
+ * Time: 08:15
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Autowired
+    private DataSource dataSource;
+}
